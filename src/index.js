7773fff24570a815d8dfa231c01d5fdf3bb2d1bb
Nadja Zollo <nadja.zollo@gmail.com>
2017-01-06 19:37:24 +0100

    Add path to JSON data and pass it as componet property.

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 import './css/index.css';
 
+const pathToRessource = 'https://gist.githubusercontent.com/luhmann/4e97da5387cec529d32ddc99784107d8/raw/bb5ce07243a9415cc9d12451ba733e23b04c58a8/mails.json';
+
 ReactDOM.render(
-  <App />,
+  <App url={pathToRessource}/>,
   document.getElementById('root')
 );
